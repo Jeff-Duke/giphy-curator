2548f8a9de94629d3b6abeb974f4932f0e83d412
Jeff Duke <jeff.duke@gmail.com>
2017-02-04 13:31:28 -0700

    base fetch function added and working

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      giphyResults: [],
+    };
+  }
+
+componentDidMount() {
+  this.fetchGiphy('cute kitten');
+}
+
+
+fetchGiphy(searchTerm) {
+  const apiKey = 'dc6zaTOxFJmzC';
+  const queryUrl = `http://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=${apiKey}`;
+
+  fetch(queryUrl)
+    .then(res => res.json())
+    .then(JSON => this.setState({ giphyResults: JSON.data }))
+    .then(console.log(this.state.giphyResults))
+    .catch(err => console.log('an error occurred: ', err));
+}
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <section className="App">
+        {this.state.giphyResults.length ? this.state.giphyResults.map(giphy => {
+          return <img src={giphy.images.downsized_medium.url} key={giphy.id} />
+        })
+        : <p>Search for a giphy</p>
+        }
+      </section>
     );
   }
 }
