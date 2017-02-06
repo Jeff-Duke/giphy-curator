981ec98323f73a3b18dfc69cd1603fcd69aa8803
Jeff Duke <jeff.duke@gmail.com>
2017-02-06 14:59:39 -0700

    refactor and clean up code a bit

--- src/App.js
+++ src/App.js
@@ -4,15 +4,10 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      giphyResults: [],
+      giphyResults: null,
       errorMessage: null,
     };
   }
-
-// componentDidMount() {
-//   this.fetchGiphy('cute kitten');
-// }
-
 
 fetchGiphy(searchTerm) {
   const apiKey = 'dc6zaTOxFJmzC';
@@ -25,7 +20,7 @@ fetchGiphy(searchTerm) {
 }
 
 copyGiphyUrl(giphyUrl) {
-  const textField = document.createElement('textarea')
+  const textField = document.createElement('textarea');
   textField.innerText = giphyUrl;
   document.body.appendChild(textField);
   textField.select();
@@ -34,23 +29,28 @@ copyGiphyUrl(giphyUrl) {
 }
 
   render() {
+    const { errorMessage, giphyResults, searchTerm } = this.state;
     return (
       <section className="App">
       <form>
+        <label htmlFor="search">Search for a Giphy:  </label>
         <input type="text" name="search" placeholder="Search for a Giphy" label="Search for a giphy" onChange={(e) => this.setState({ searchTerm: e.target.value }) } />
-        <button type="submit" children="Search" disabled={!this.state.searchTerm} 
+        <button type="submit" children="Search" disabled={!searchTerm}
         onClick={(e) => {
           e.preventDefault();
-          this.fetchGiphy(this.state.searchTerm);
+          this.fetchGiphy(searchTerm);
         }} />
       </form>
 
-        {this.state.errorMessage && <p>{ this.state.errorMessage } </p>}
+        {errorMessage && <p>{ errorMessage } </p>}
 
-        {this.state.giphyResults.length ? this.state.giphyResults.map(giphy => {
-          return <img src={giphy.images.downsized_medium.url} key={giphy.id} onClick={(e) => this.copyGiphy(e.target.src)}/>
-        })
-        : <p>Search for a giphy</p>
+        {giphyResults && giphyResults.map(giphy => {
+          return <img key={giphy.id}
+                      role="presentation"
+                      src={giphy.images.downsized_medium.url}
+                      onClick={(e) => this.copyGiphyUrl(e.target.src)}
+                  />;
+          })
         }
       </section>
     );
