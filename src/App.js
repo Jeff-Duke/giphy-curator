ee760d09507f7a6d8e6b53ac077d3308bea94c2b
Jeff Duke <jeff.duke@gmail.com>
2017-02-06 16:11:05 -0700

    add an error message output and user feedback output

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ class App extends Component {
     this.state = {
       giphyResults: null,
       errorMessage: null,
+      searchTerm: null,
+      userFeedback: null,
     };
   }
 
@@ -16,31 +18,50 @@ fetchGiphy(searchTerm) {
   fetch(queryUrl)
     .then(res => res.json())
     .then(JSON => this.setState({ giphyResults: JSON.data }))
-    .catch(err => console.log('an error occurred: ', err));
+    .catch((err) => {
+      console.log('an error occurred: ', err);
+      this.displayAnError('An error occurred getting those giphy\'s, please try again');
+    });
+}
+
+giveUserFeedback(feedback) {
+  this.setState({ userFeedback: feedback });
+  setTimeout(() => { this.setState({ userFeedback: null }); }, 500);
+}
+
+displayAnError(error) {
+  this.setState({ errorMessage: error});
+  setTimeout(() => { this.setState({ errorMessage: null }); }, 2000);
 }
 
 copyGiphyUrl(giphyUrl) {
+  this.state.userFeedback && this.setState({ userFeedback: null });
   const textField = document.createElement('textarea');
   textField.innerText = giphyUrl;
   document.body.appendChild(textField);
   textField.select();
   document.execCommand('copy');
   textField.remove();
+  this.giveUserFeedback('copied to clipboard');
 }
 
   render() {
-    const { errorMessage, giphyResults, searchTerm } = this.state;
+    const { errorMessage, userFeedback, giphyResults, searchTerm } = this.state;
     return (
       <section className="App">
       <form>
         <label htmlFor="search">Search for a Giphy:  </label>
         <input type="text" name="search" placeholder="Search for a Giphy" label="Search for a giphy" onChange={(e) => this.setState({ searchTerm: e.target.value }) } />
-        <button type="submit" children="Search" disabled={!searchTerm}
-        onClick={(e) => {
-          e.preventDefault();
-          this.fetchGiphy(searchTerm);
-        }} />
+        <button type="submit"
+          children="Search"
+          disabled={!searchTerm}
+          onClick={(e) => {
+            e.preventDefault();
+            errorMessage && this.setState({ errorMessage: null });
+            this.fetchGiphy(searchTerm);
+          }} />
       </form>
+        {userFeedback && <p>{ userFeedback }</p>}
 
         {errorMessage && <p>{ errorMessage } </p>}
 
