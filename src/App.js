e7ba7c76a2125f04d4344880c8357426f00d437c
Jeff Duke <jeff.duke@gmail.com>
2017-02-06 14:36:32 -0700

    add a search form to fetch giphys

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ class App extends Component {
     super();
     this.state = {
       giphyResults: [],
+      errorMessage: null,
     };
   }
 
-componentDidMount() {
-  this.fetchGiphy('cute kitten');
-}
+// componentDidMount() {
+//   this.fetchGiphy('cute kitten');
+// }
 
 
 fetchGiphy(searchTerm) {
@@ -20,15 +21,34 @@ fetchGiphy(searchTerm) {
   fetch(queryUrl)
     .then(res => res.json())
     .then(JSON => this.setState({ giphyResults: JSON.data }))
-    .then(console.log(this.state.giphyResults))
     .catch(err => console.log('an error occurred: ', err));
+}
+
+copyGiphyUrl(giphyUrl) {
+  const textField = document.createElement('textarea')
+  textField.innerText = giphyUrl;
+  document.body.appendChild(textField);
+  textField.select();
+  document.execCommand('copy');
+  textField.remove();
 }
 
   render() {
     return (
       <section className="App">
+      <form>
+        <input type="text" name="search" placeholder="Search for a Giphy" label="Search for a giphy" onChange={(e) => this.setState({ searchTerm: e.target.value }) } />
+        <button type="submit" children="Search" disabled={!this.state.searchTerm} 
+        onClick={(e) => {
+          e.preventDefault();
+          this.fetchGiphy(this.state.searchTerm);
+        }} />
+      </form>
+
+        {this.state.errorMessage && <p>{ this.state.errorMessage } </p>}
+
         {this.state.giphyResults.length ? this.state.giphyResults.map(giphy => {
-          return <img src={giphy.images.downsized_medium.url} key={giphy.id} />
+          return <img src={giphy.images.downsized_medium.url} key={giphy.id} onClick={(e) => this.copyGiphy(e.target.src)}/>
         })
         : <p>Search for a giphy</p>
         }
