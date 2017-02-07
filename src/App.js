546687fedcb265d1babe92bf78ee21723ebaef50
Jeff Duke <Jeff.Duke@Gmail.com>
2017-02-06 20:32:23 -0700

    Add a Header component (#7)
    * add a header component and break out app component
    
    * refactor state changes
    
    * refactor and clean up.  move feedback and error message to header

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React, { Component } from 'react';
+import Header from './Header';
 
-class App extends Component {
+export default class App extends Component {
   constructor() {
     super();
     this.state = {
-      giphyResults: null,
+      giphySearchResults: null,
       errorMessage: null,
-      searchTerm: null,
       userFeedback: null,
-      resultCount: 5,
-      rating: 'g',
     };
   }
 
-fetchGiphy(searchTerm) {
-  const { resultCount, rating } = this.state;
-  const apiKey = 'dc6zaTOxFJmzC';
-  const queryUrl = `http://api.giphy.com/v1/gifs/search?q=${searchTerm}&limit=${resultCount}&rating=${rating}&api_key=${apiKey}`;
+fetchGiphy(queryUrl) {
+  this.setState({ errorMessage: null });
 
   fetch(queryUrl)
     .then(response => response.json())
-    .then(JSONResponse => this.setState({ giphyResults: JSONResponse.data }))
+    .then(JSONResponse => this.setState({ giphySearchResults: JSONResponse.data }))
     .catch((error) => {
       console.log('an error occurred: ', error);
       this.displayAnError('An error occurred getting those giphy\'s, please try again');
@@ -29,7 +25,7 @@ fetchGiphy(searchTerm) {
 
 giveUserFeedback(feedback) {
   this.setState({ userFeedback: feedback });
-  setTimeout(() => { this.setState({ userFeedback: null }); }, 500);
+  setTimeout(() => { this.setState({ userFeedback: null }); }, 1000);
 }
 
 displayAnError(error) {
@@ -38,74 +34,34 @@ displayAnError(error) {
 }
 
 copyGiphyUrl(giphyUrl) {
-  this.state.userFeedback && this.setState({ userFeedback: null });
+  this.setState({ userFeedback: null });
   const textField = document.createElement('textarea');
   textField.innerText = giphyUrl;
   document.body.appendChild(textField);
   textField.select();
   document.execCommand('copy');
   textField.remove();
-  this.giveUserFeedback('copied to clipboard');
+  this.giveUserFeedback('Link copied to clipboard');
 }
 
   render() {
-    const { errorMessage, userFeedback, giphyResults, searchTerm, resultCount, rating } = this.state;
+    const { giphySearchResults, errorMessage, userFeedback } = this.state;
     return (
-      <section className="App">
-        <form>
-          <label htmlFor="search">  Search for a Giphy:
-            <input className="SearchInput"
-                    type="text"
-                    name="search"
-                    placeholder="Search for a Giphy"
-                    label="Search for a giphy"
-                    onChange={(e) => this.setState({ searchTerm: e.target.value }) }
-            />
-          </label>
-          <button type="submit"
-            children="Search"
-            disabled={!searchTerm}
-            onClick={(e) => {
-              e.preventDefault();
-              errorMessage && this.setState({ errorMessage: null });
-              this.fetchGiphy(searchTerm);
-            }}
-          />
-        </form>
-        <label htmlFor="result-count">  Number of results (max: 100):
-            <input className="ResultCount"
-                  type="number"
-                  name="result-count"
-                  min="0"
-                  max="100"
-                  value={resultCount}
-                  onChange={(e) => this.setState({ resultCount: e.target.value })}
-            />
-        </label>
-        <label htmlFor="rating-picker"> Giphy Rating:
-          <select name="rating-picker" id="rating-picker" value={rating} onChange={(e) => this.setState({ rating: e.target.value })}>
-            <option value="y">Y</option>
-            <option value="g">G</option>
-            <option value="pg">PG</option>
-            <option value="pg-13">PG-13</option>
-            <option value="r">R</option>
-          </select>
-        </label>
-          <p>{ userFeedback }</p>
-
-          <p>{ errorMessage }</p>
-
-          {giphyResults && giphyResults.map(giphy => {
-            return <img key={giphy.id}
-                        role="presentation"
-                        src={giphy.images.downsized_medium.url}
-                        onClick={(e) => this.copyGiphyUrl(e.target.src)}
-                    />;
-            })
-          }
+      <section className='App'>
+      <Header handleSubmit={(queryUrl) => this.fetchGiphy(queryUrl)}
+              errorMessage={errorMessage}
+              userFeedback={userFeedback}
+      />
+      {giphySearchResults && giphySearchResults.map(giphy => {
+        return <img key={giphy.id}
+                    role='presentation'
+                    src={giphy.images.fixed_height.url}
+                    onClick={(e) => this.copyGiphyUrl(e.target.src)}
+                />;
+        })
+      }
       </section>
     );
   }
 }
 
-export default App;
---
+++ src/Header.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+  constructor() {
+    super();
+    this.state = {
+      searchTerm: null,
+      resultCount: 5,
+      rating: 'g'
+    };
+  }
+
+  createGiphyQueryUrl() {
+  const { searchTerm, resultCount, rating } = this.state;
+  const apiKey = 'dc6zaTOxFJmzC';
+  const queryUrl = `http://api.giphy.com/v1/gifs/search?q=${searchTerm}&limit=${resultCount}&rating=${rating}&api_key=${apiKey}`;
+
+  this.props.handleSubmit(queryUrl);
+}
+
+  render() {
+    const { searchTerm, resultCount, rating } = this.state;
+    const { userFeedback, errorMessage } = this.props;
+    return(
+      <section className='Header'>
+        <form>
+          <label htmlFor='search'>  Search for a Giphy:
+            <input className='SearchInput'
+                    type='text'
+                    name='search'
+                    placeholder='Search for a Giphy'
+                    label='Search for a giphy'
+                    onChange={(e) => this.setState({ searchTerm: e.target.value }) }
+            />
+          </label>
+          <button type='submit'
+            children='Search'
+            disabled={!searchTerm}
+            onClick={(e) => {
+              e.preventDefault();
+              this.createGiphyQueryUrl();
+            }}
+          />
+        </form>
+        <label htmlFor='result-count'>  Number of results (max: 100):
+            <input className='ResultCount'
+                  type='number'
+                  name='result-count'
+                  min='0'
+                  max='100'
+                  value={resultCount}
+                  onChange={(e) => this.setState({ resultCount: e.target.value })}
+            />
+        </label>
+        <label htmlFor='rating-picker'> Giphy Rating:
+          <select name='rating-picker' id='rating-picker' value={rating} onChange={(e) => this.setState({ rating: e.target.value })}>
+            <option value='y'>Y</option>
+            <option value='g'>G</option>
+            <option value='pg'>PG</option>
+            <option value='pg-13'>PG-13</option>
+            <option value='r'>R</option>
+          </select>
+        </label>
+        <p className='HeaderError'>{ errorMessage }</p>
+        <p className='HeaderFeedback'>{ userFeedback }</p>
+      </section>
+    )
+  }
+}
